02dbdedb5a79c4478513479ef11bc26d461d1f10
Aaryalegend <babararyan5796@gmail.com>
2024-02-06 17:09:10 +0530

    Added lifecycle

---
+++ aryan_lifecycle/app/src/main/java/com/aryanbabar/aryan_lifecycle/MainActivity.java
@@ -0,0 +1,62 @@
+package com.aryanbabar.aryan_lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Message", "onCreate");
+
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Message", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Message", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Message", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Message", "onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Message", "onCreate");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Message", "onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+}
